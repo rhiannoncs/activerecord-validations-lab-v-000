9200732ef226ec5b3aa1ae5894306cfacef95ccb
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-03-22 17:45:57 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, format: {with: /Won't Believe|Secret|Top \d+|Guess/}
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
